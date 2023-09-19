4200abaa80f4c86f50c35b76cce6c3e73814d1e7
sulayman-ma <suleyman.abba@gmail.com>
2023-09-19 03:24:16 +0100

    switched to SQLite for this branch

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SECRET_KEY = 'asdmnc5b4734-+*23'
     # CSRF_TOKEN = 'fmn3c7(#&(3misaER'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:%Prometheus123@localhost/estate'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///render.db'
     API_KEY = 'sk_test_3cc377fb007fc4608e1c25f0e6810bf976c7af65'
     # need this for some weird reason I cannot remember
     @staticmethod
